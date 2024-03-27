9cafaf6ba9de3faebdf4687beb86c57678075a33
Abdul <abdul.tanveer@gmail.com>
2024-03-27 11:21:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/scb7/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.scb7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun clickHandler(view: View) {
+        var tv: TextView = findViewById(R.id.textview)
+        var etName:EditText = findViewById(R.id.etName)
+        var name = etName.text.toString()
+        tv.setText(name)
+
+
+    }
+}
