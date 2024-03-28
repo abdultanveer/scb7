7fa30d238cc17819ce309dd9c82f66fa81bc9178
Abdul <abdul.tanveer@gmail.com>
2024-03-28 08:57:00 +0530

    recyclerview -1

---
+++ app/src/main/java/com/example/scb7/RecyclerActivity.kt
@@ -0,0 +1,17 @@
+package com.example.scb7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerActivity : AppCompatActivity() {
+
+    var countries = arrayOf("India","USA","UK","Russia","China")
+    lateinit var cRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler)
+        cRecyclerView = findViewById(R.id.recyclerView)
+    }
+}
